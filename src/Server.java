bf2beb448dd609e1d11a37a3bf45a5fdbf3eb7b3
Robert Prast <rprast@newrelic.com>
2021-02-05 14:35:29 -0500

    a

--- src/Server.java
+++ src/Server.java
@@ -7,7 +7,6 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
-// Read the full article https://dev.to/mateuszjarzyna/build-your-own-http-server-in-java-in-less-than-one-hour-only-get-method-2k02
 public class Server {
 
     public static void main( String[] args ) throws Exception {

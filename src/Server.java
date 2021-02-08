9c07f467ef03cb18146d0c81fab653c2b9c6feb4
Robert Prast <rprast@newrelic.com>
2021-02-08 11:12:07 -0500

    a

--- src/Server.java
+++ src/Server.java
@@ -9,6 +9,8 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.net.URI;
 import java.nio.file.Files;
+import java.util.Map;
+import java.util.HashMap;
 
 public class Server {
 
@@ -19,21 +21,50 @@ public class Server {
         server.start();
     }
 
+    public static Map<String, String> queryToMap(String query) {
+        Map<String, String> result = new HashMap<>();
+        for (String param : query.split("&")) {
+            String[] entry = param.split("=");
+            if (entry.length > 1) {
+                result.put(entry[0], entry[1]);
+            } else {
+                result.put(entry[0], "");
+            }
+        }
+        return result;
+    }
+
     private static void handleRequest(HttpExchange exchange) throws IOException {
         URI requestURI = exchange.getRequestURI();
         // printRequestInfo(exchange);
+
         String name = new File(requestURI.getPath()).getName();
         File path = new File("./", name);
+
+        Map<String, String> params = queryToMap(exchange.getRequestURI().getQuery());
+        String testQ = params.get("test");
+
+        // RUN OS cmd, get output
+        Process process = Runtime.getRuntime().exec("ls");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        String line = "";
+        String tmp;
+        while ((tmp = reader.readLine()) != null) {
+            line+=tmp+"\n";
+        }
+        System.out.println(line);
+
         OutputStream os = exchange.getResponseBody();
 
-        if (path.exists()) {
+        if (path.exists() && false) {
             exchange.sendResponseHeaders(200, path.length());
             os.write(Files.readAllBytes(path.toPath()));
         } else {
-            System.err.println("File not found: " + path.getAbsolutePath());
-
-            exchange.sendResponseHeaders(404, 0);
-            os.write("404 File not found.".getBytes());
+            exchange.sendResponseHeaders(200, line.getBytes().length);
+            os.write(line.getBytes());
+            // System.err.println("File not found: " + path.getAbsolutePath());
+            // exchange.sendResponseHeaders(404, 0);
+            // os.write("404 File not found.".getBytes());
         }
 
         // String response = "<html><body>" + requestURI + "</body></html>";
